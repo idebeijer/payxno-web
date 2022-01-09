70384e9cec8c2ad50b1f10825b81e024004a2cff
idebeijer <idb.debeijer@gmail.com>
2022-01-09 15:23:33 +0100

    gh-pages predeploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
