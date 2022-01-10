41d8d14860bfe7307db5bde6f22228b149c899fb
idebeijer <idb.debeijer@gmail.com>
2022-01-10 18:26:24 +0100

    removed gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
