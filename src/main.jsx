369a02f56794aeb4b1bfd7ca512b12d1877a57b9
idebeijer <idb.debeijer@gmail.com>
2022-04-20 17:29:25 +0200

    upgrade react

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <RecoilRoot>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
